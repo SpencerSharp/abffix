0fdfca3a6a513432695a79b06962370dc20efc18
Spencer Sharp <spencersharp@me.com>
2019-10-23 18:14:32 -0500

    Applies to all teams

--- try.py
+++ try.py
@@ -12,8 +12,15 @@ with open(old_export_filename,encoding='utf-8-sig') as oldexportfile:
         new = pd.read_json('['+newexportfile.read()+']',orient='records')
 
         changed = new.copy()
-        old_colors = old['teams'][0][0]['colors']
-        changed['teams'][0][0]['colors'] = old_colors
+        old_colors = (old['teams'][0][x]['colors'] for x in range(0,len(old['teams'][0])))
+        ind = 0
+        try:
+            while(True):
+                changed['teams'][0][ind]['colors'] = next(old_colors)
+                ind+=1
+        except StopIteration:
+            pass
+
         with open('fixedexport.json','w+',encoding='utf-8-sig') as fixedexport:
             temp = changed.to_json(orient='records')
             temp = temp[1:-1]
